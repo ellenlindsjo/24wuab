8255b630d639b221e436f65b213a38f255669134
ellenlindsjo <ellen.lindsjo@elev.ga.ntig.se>
2024-03-09 19:12:21 +0100

    2024-03-09
    Efter studiedag. Har lagt in text som ska vara med, men kanske får ändra lite till på den. Försökte fixa med hamburgermenyn.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
-document.querySelector(".fade-layer").addEventListener("click", showMenu)
+document.querySelector(".fade-layer").addEventlistener("click",showMenu)
 
-document.querySelector(".menu-button").addEventListener("click", showMenu)
-
-document.querySelector("material-symbols-outlined").addEventListener("click", hideMenu)
-
-document.querySelector(".fade-layer").addEventListener("click", hideMenu)
+document.querySelector(".menuButton").addEventlistener("click",showMenu)
 
 function showMenu(){
-    let menu = document.querySelector("nav.menu")
-    menu.classList.toggle("show")
+    let menu = document.querySelector("nav.menu");
+    menu.classList.toggle("show");
 
-    let layer = document.querySelector(".fade-layer")
-    layer.classList.toggle("visible")
-}
-
-function hideMenu(){
-    let menu = document.querySelector("nav.menu")
-    menu.classList.toggle("hide")
-
-    let layer = document.querySelector(".fade-layer")
-    layer.classList.toggle("invisible")
+    let layer = document.querySelector(".fade-layer");
+    layer.classList.toggle("visible");
 }
