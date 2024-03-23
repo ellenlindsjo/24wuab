d1c0b4ccd26a049e16e8953b038cba0f33959c2d
ellenlindsjo <ellen.lindsjo@elev.ga.ntig.se>
2024-03-23 23:10:09 +0100

    2024-03-23
    Validering av koden.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-document.querySelector(".fade-layer").addEventListener("click",toggleMenu)
+document.querySelector(".fade-layer").addEventListener("click",toggleMenu);
 
-document.querySelector(".menu-button").addEventListener("click",toggleMenu)
+document.querySelector(".menu-button").addEventListener("click",toggleMenu);
 
-document.querySelector(".material-symbols-outlined").addEventListener("click",toggleMenu)
+document.querySelector(".material-symbols-outlined").addEventListener("click",toggleMenu);
 
 function toggleMenu(){
     let menu = document.querySelector("nav.menu");
